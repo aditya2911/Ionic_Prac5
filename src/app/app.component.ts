d7544609b7054b97ef55e3a224f2794d1f80e194
Aditya Salunke <adisalu15@gmail.com>
2023-05-04 07:46:27 +0530

    started working on splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform:Platform,
+    public router:Router,
+  ) {
+    this.initializeApp();
+  }
+  initializeApp() {
+      this.platform.ready().then(()=>{
+        this.router.navigateByUrl('tabs ');
+      })
+  }
 }
