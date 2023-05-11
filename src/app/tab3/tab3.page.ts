f41c41ddee8ab89f7d59aa80585b121073030e48
Aditya Salunke <adisalu15@gmail.com>
2023-05-11 12:17:55 +0530

    added leaderboards screen

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
 
+interface AccordionItem {
+  title: string;
+  expanded: boolean;
+}
+
+interface ListItem{
+  name:String,
+  designation:String,
+  email:String,
+  phone:String,
+  domain:String,
+  image:String,
+
+}
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -8,5 +23,83 @@ import { Component } from '@angular/core';
 export class Tab3Page {
 
   constructor() {}
+  items :ListItem[] = [
+    {
+      name:"Dr. Dhananjay Kalbande",
+      designation:"Head Of Department",
+      email:"drkalbande@spit.ac.in",
+      phone:"+91 (22) – 26707440 ",
+      domain:"MCA",
+      image:"https://cse.spit.ac.in/assets/faculty/dr-dhananjay-kalbande.jpg"
+    },
+    {
+    name:"Prof. Harshil Kanakia",
+    designation:"Asst. Professor",
+    email:"harshil.kanakia@gmail.com",
+    phone:"+91 (22) – 26707440",
+    domain:"MCA",
+    image:"https://mca.spit.ac.in/wp-content/uploads/2022/12/HK.png"
+  },
+  {
+    name:"Prof. Nikhita Mangaonkar",
+    designation:"Asst. Professor",
+    email:"nikhita.mangaonkar@spit.ac.in",
+    phone:"+91 (22) – 26707440",
+    domain:"MCA",
+    image:"https://mca.spit.ac.in/wp-content/uploads/2022/12/NM.jpg"
+  },
+  {
+    name:"Prof. Dr. Pooja Raundale",
+    designation:"Professor",
+    email:"pooja@spit.ac.in",
+    phone:"+91 (22) – 26707440",
+    domain:"MCA",
+    image:"https://mca.spit.ac.in/wp-content/uploads/2022/12/PR-e1670491866552.jpg"
+  },
+  {
+    name:"Prof. S G Vaidya",
+    designation:"Asst. Professor",
+    email:"sampat.vaidya@spit.ac.in",
+    phone:"+91 (22) – 26707440",
+    domain:"MCA",
+    image:"https://mca.spit.ac.in/wp-content/uploads/2022/12/SG-150x150.png"
+  }
+  ,
+  {
+    name:"Prof. Pallavi Thakur",
+    designation:"Asst. Professor",
+    email:"pallavi.thakur@spit.ac.in",
+    phone:"+91 (22) – 26707440",
+    domain:"MCA",
+    image:"https://mca.spit.ac.in/wp-content/uploads/2022/12/PT-150x150.png"
+  }
+  ,
+  {
+    name:"Prof. Dr. Aarti Karande",
+    designation:"Asst. Professor",
+    email:"aarti.karande@spit.ac.in",
+    phone:"+91 (22) – 26707440",
+    domain:"MCA",
+    image:"https://mca.spit.ac.in/wp-content/uploads/2022/12/aarti.png"
+  }
+  ,
+  {
+    name:"Prof. Sonali Wankhede",
+    designation:"Asst. Professor",
+    email:"sonali.wankhede@spit.ac.in",
+    phone:"+91 (22) – 26707440",
+    domain:"MCA",
+    image:"https://mca.spit.ac.in/wp-content/uploads/2022/12/SW-150x150.png"
+  }
+  ,
+  {
+    name:"Prof. Sparsh Vyas",
+    designation:"Asst. Professor",
+    email:"sparsh.vyas@spit.ac.in",
+    phone:"+91 (22) – 26707440",
+    domain:"MCA",
+    image:"https://mca.spit.ac.in/wp-content/uploads/2022/12/SV-150x150.png"
+  }
+  ];
 
 }
